c40d5bc7b9f06c39297e11801b204de7bdc80822
snert <snert@snert-lab.de>
2010-05-13 17:16:56 +0200

    Fix: right click didn´t enabled the download menu

--- src/de/snertlab/xdccBee/ui/FileViewerMenu.java
+++ src/de/snertlab/xdccBee/ui/FileViewerMenu.java
@@ -19,12 +19,9 @@ package de.snertlab.xdccBee.ui;
 
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.action.MenuManager;
-import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.widgets.Event;
-import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.events.MouseAdapter;
+import org.eclipse.swt.events.MouseEvent;
 
-import de.snertlab.xdccBee.irc.DccPacket;
 import de.snertlab.xdccBee.ui.actions.ActionDownloadFile;
 
 
@@ -35,19 +32,17 @@ public class FileViewerMenu {
 		downloadAction.setEnabled(false);
 		contextMenu.add(downloadAction);
 		fileViewer.getControl().setMenu(contextMenu.createContextMenu(fileViewer.getControl()));
-		fileViewer.getTable().addListener(SWT.Show, new Listener() {	
+		fileViewer.getTable().addMouseListener( new MouseAdapter() {
 			@Override
-			public void handleEvent(Event event) {
-				if(getSelectedFileItem(fileViewer) == null) {
-					downloadAction.setEnabled(false);
-				} else {
-					downloadAction.setEnabled(true);
+			public void mouseDown(MouseEvent e) {
+				if(e.button == 3){
+					if(fileViewer.getSelectedDccPacket() == null) {
+						downloadAction.setEnabled(false);
+					} else {
+						downloadAction.setEnabled(true);
+					}					
 				}
-			}			
+			}
 		});
-	}
-	public DccPacket getSelectedFileItem(FileViewer packetViewer) {
-		IStructuredSelection selectedItem = (IStructuredSelection) packetViewer.getSelection();
-		return (DccPacket)selectedItem.getFirstElement();
-	}
+	}	
 }
